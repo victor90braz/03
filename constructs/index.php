e0378c58948979ea94a08ff20767760be5a06297
victor braz <victor.90braz@gmail.com>
2023-10-10 18:41:37 +0200

    get collection of videos

--- constructs/index.php
+++ constructs/index.php
@@ -16,4 +16,38 @@ class SpecialtyCoffeeMaker extends CoffeMaker {
     }
 }
 
-(new SpecialtyCoffeeMaker())->brewLatte();
+// (new SpecialtyCoffeeMaker())->brewLatte();
+
+class Collection {
+
+    protected array $items;
+
+    function __construct($items = []) {
+        $this->items = $items;
+    }
+
+    public function sum($key) {
+        return array_sum(array_map(function($item) use ($key) {
+            return $item->$key;
+        }, $this->items));
+    }
+}
+
+class Video {
+
+    public $title;
+    public $length;
+
+    public function __construct($title, $length) {
+        $this->title = $title;
+        $this->length = $length;
+    }
+}
+
+$collection = new Collection([
+    new Video('Hexa Flamengo 2019', 100),
+    new Video('Penta Brasil 2002', 200),
+    new Video('Copa do Brasil 2004', 500),
+]);
+
+dd($collection->sum('length'));
